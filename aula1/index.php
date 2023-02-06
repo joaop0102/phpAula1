2ce9911725381aa5d54349c340c44c7cdc3160b2
joaop0102 <101288165+joaop0102@users.noreply.github.com>
2023-02-06 16:31:13 -0300

    Add files via upload
    primeira de PHP com exemplos

---
+++ aula1/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+$a = 1;
+
+$b = 2;
+
+echo "Adição :" . $a + $b . "<br>";
+
+echo "Subtração :" . $a - $b . "<br>";
+
+echo "Multiplicação :" . $a * $b . "<br>";
+
+echo "Divisão :" . $a / $b . "<br>";
+
+?>
